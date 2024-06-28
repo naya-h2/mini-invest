abf756689e3cfc19e945e4400e5aeefe3ec07950
Heewon <hw7666@sogang.ac.kr>
2024-06-29 00:04:55 +0900

    ✨ feat: list 페이지 api 연결 완료

--- src/pages/list/ListPage.tsx
+++ src/pages/list/ListPage.tsx
@@ -2,15 +2,44 @@ import styled from "styled-components";
 import BottomNav from "../../components/BottomNav";
 import Header from "../../components/Header";
 import CardList from "../../components/list/CardList";
-import { POPULAR_DATA } from "../../constants/mockup";
+import { useSearchParams } from "react-router-dom";
+import { useQuery } from "react-query";
+import axios from "axios";
+import { PROXY } from "../../constants/api";
 
 function ListPage() {
-  //query로 category 사용
+  const [searchParams, setSearchParams] = useSearchParams();
+  const category = searchParams.get("category");
+  const end = searchParams.get("end") === "true";
+  const keyword = searchParams.get("keyword");
+
+  const makeRequestUrl = () => {
+    if (category) return `/api/category?category=${category}`;
+    if (end) return `/api/end`;
+    return `/api/keyword?keyword=${keyword}`;
+  };
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["item-list", category, end, keyword],
+    queryFn: async () => {
+      const url = makeRequestUrl();
+      const res = await axios.get(PROXY + url);
+      return res.data;
+    },
+  });
+
+  const makeTitleText = () => {
+    if (category) return `지금 투자 가능한 [ ${category} ] 목록입니다.`;
+    if (end) return `최근 종료된 투자 목록입니다.`;
+    return `[ ${keyword} ] 로 검색한 결과입니다.`;
+  };
+
   return (
     <>
       <Header />
       <Content>
-        <CardList data={POPULAR_DATA} />
+        <Title>{makeTitleText()}</Title>
+        {isLoading ? "로딩중.." : data && data.length > 0 ? <CardList data={data} isEnd={end} /> : "데이터가 없습니다."}
       </Content>
       <BottomNav />
     </>
@@ -21,5 +50,13 @@ export default ListPage;
 
 const Content = styled.div`
   margin: 54px 0 96px;
-  padding: 0 20px 20px;
+  padding: 10px 20px 20px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+const Title = styled.p`
+  font-weight: 500;
 `;
