8bfe5e4073a5133a69af05a34082f02dd7ac28cd
Heewon <hw7666@sogang.ac.kr>
2024-10-29 00:36:43 +0900

    ✨ feat: invest 페이지 API 연결

--- src/pages/invest/InvestPage.tsx
+++ src/pages/invest/InvestPage.tsx
@@ -7,77 +7,66 @@ import { Helmet } from "react-helmet-async";
 import categoryIcon from "../../assets/circle_right.svg";
 import sellerIcon from "../../assets/seller.svg";
 
-const data = {
-  id: 19,
-  name: "배추(15000평)",
-  description: "농장 위치: 충북 괴산",
-  category: "농산물",
-  rate: 7.84,
-  yearRate: 7.86,
-  investNum: 72,
-  startDate: "2024-09-01",
-  endDate: "2024-12-01",
-  method: "펀딩",
-  price: 72000,
-  imgUrl:
-    "https://hilarious-snapdragon-2ae.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Fde448d9f-35f7-4ad6-850b-8de1c0d77406%2F7ff2aff2-85c9-4666-a013-3f8b9140b481%2Fimage.png?table=block&id=12b8863b-8c59-808f-832e-ec9024f21f6e&spaceId=de448d9f-35f7-4ad6-850b-8de1c0d77406&width=2000&userId=&cache=v2",
-  dday: 34,
-};
-
 function InvestPage() {
   const params = useParams();
   const investId = params.id;
 
-  // const { data, isLoading } = useQuery({
-  //   queryKey: ["item", investId],
-  //   queryFn: async () => {
-  //     const res = await axios.get(`${PROXY}/api/`);
-  //     if (res.data.length > 4) return res.data.slice(0, 4);
-  //     return res.data;
-  //   },
-  // });
+  const { data, isLoading } = useQuery({
+    queryKey: ["item", investId],
+    queryFn: async () => {
+      const res = await axios.get(`${PROXY}/api?id=${investId}`);
+
+      return res.data;
+    },
+  });
 
   return (
-    <div>
-      <Helmet>
-        <title>{data.name} | 펀딩</title>
-      </Helmet>
-      <ProductImg src={data.imgUrl} alt="상품 이미지" />
-      <Container>
-        <CategoryWrapper onClick={() => (window.location.href = `/list?category=${data.category}`)}>
-          {data.category}
-          <img src={categoryIcon} alt="카테고리 바로가기" />
-        </CategoryWrapper>
+    <>
+      {isLoading ? (
+        <div>로딩중..</div>
+      ) : (
+        <>
+          <Helmet>
+            <title>{data.name} | 펀딩</title>
+          </Helmet>
+          <ProductImg src={data.imgUrl} alt="상품 이미지" />
+          <Container>
+            <CategoryWrapper onClick={() => (window.location.href = `/list?category=${data.category}`)}>
+              {data.category}
+              <img src={categoryIcon} alt="카테고리 바로가기" />
+            </CategoryWrapper>
 
-        <Title>{data.name}</Title>
-        <Description>{data.description}</Description>
+            <Title>{data.name}</Title>
+            <Description>{data.description}</Description>
 
-        <Price $color={"#00BF40"}>
-          {data.price.toLocaleString("ko-kr")}
-          <MiniLetter>원</MiniLetter>
-        </Price>
-        <Price $isDown>
-          {data.investNum}
-          <MiniLetter>명 참여</MiniLetter>
-        </Price>
+            <Price $color={"#00BF40"}>
+              {data.price.toLocaleString("ko-kr")}
+              <MiniLetter>원</MiniLetter>
+            </Price>
+            <Price $isDown>
+              {data.investNum}
+              <MiniLetter>명 참여</MiniLetter>
+            </Price>
 
-        <MiniLetter>판매자 정보</MiniLetter>
-        <Seller>
-          <LeftBox>
-            <img src={sellerIcon} alt="판매자 아이콘" />
-            <div>
-              주식회사 레어
-              <br />
-              <Follower>364 팔로워</Follower>
-            </div>
-          </LeftBox>
-          <FollowBtn>팔로우</FollowBtn>
-        </Seller>
-      </Container>
-      <BottomBox>
-        <BottomBtn>펀딩하기</BottomBtn>
-      </BottomBox>
-    </div>
+            <MiniLetter>판매자 정보</MiniLetter>
+            <Seller>
+              <LeftBox>
+                <img src={sellerIcon} alt="판매자 아이콘" />
+                <div>
+                  주식회사 레어
+                  <br />
+                  <Follower>364 팔로워</Follower>
+                </div>
+              </LeftBox>
+              <FollowBtn>팔로우</FollowBtn>
+            </Seller>
+          </Container>
+          <BottomBox>
+            <BottomBtn>펀딩하기</BottomBtn>
+          </BottomBox>
+        </>
+      )}
+    </>
   );
 }
 
